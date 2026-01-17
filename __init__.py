a26582b1f12f59a08d3487ebf4c8bea9ac5faead
tarcon <git@grgr.dev>
2026-01-17 16:32:28 +0100

    format

--- __init__.py
+++ __init__.py
@@ -8,4 +8,3 @@ __all__ = [
 __author__ = """ComfyUI-Sharpfin"""
 __email__ = "git@grgr.dev"
 __version__ = "0.0.1"
-
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,6 @@ import sys
 import os
 
 # Füge den 'lib' Ordner zum Pfad hinzu, damit 'sharpfin' als Top-Level-Modul gefunden wird
-lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib'))
+lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "lib"))
 if lib_path not in sys.path:
     sys.path.insert(0, lib_path)
