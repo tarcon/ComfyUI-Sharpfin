d2910454b0d5c45fcd5d0e6c6fbdb45912496a7b
gregor <git@grgr.dev>
2026-01-17 10:46:40 +0100

    scaffold project with cookiecutter

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for sharpfin."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+
+]
+
+__author__ = """ComfyUI-Sharpfin"""
+__email__ = "git@grgr.dev"
+__version__ = "0.0.1"
+
+from .src.sharpfin.nodes import NODE_CLASS_MAPPINGS
+from .src.sharpfin.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
